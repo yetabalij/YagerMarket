b0f7b63f09ca64fc260af185f7bf415d402600e3
yetabalij <yetabalij@gmail.com>
2022-09-13 15:37:40 +0300

    db connection established.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 dotenv.config();
 
 //creating the express app
@@ -10,8 +11,19 @@ app.get("/", (req, res) => {
   res.send("Hello ermi");
 });
 
+//Connecting to mongoose db
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MongoDb);
+    console.log("Database Connected.");
+  } catch (error) {
+    throw error;
+  }
+};
+
 //Creating the server
 const Port = process.env.PORT;
 app.listen(Port || 5000, () => {
+  connect();
   console.log(`app is started at ${Port}`);
 });
