18a3471d45a819d43a49de17c79faede09205c92
yetabalij <yetabalij@gmail.com>
2022-09-14 15:49:43 +0300

    routes imported.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 dotenv.config();
+
+//route imports
+const productRoute = require("./routes/product");
 
 //creating the express app
 const app = express();
 
+//middelwares
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(cors({ origin: true, credentials: true }));
+app.use(morgan("dev"));
+
 //routing midlewars
-app.get("/", (req, res) => {
-  res.send("Hello ermi");
-});
+app.use("/api/product", productRoute);
 
 //Connecting to mongoose db
 const connect = async () => {
