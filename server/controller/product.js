0c297080591b3c181b2ecf185f0444b562087ed0
yetabalij <yetabalij@gmail.com>
2022-09-14 15:51:24 +0300

    create controller completed and get controller started.

---
+++ server/controller/product.js
@@ -0,0 +1,21 @@
+const Products = require("./../model/Product");
+
+exports.createProduct = async (req, res) => {
+  const newProduct = Products(req.body);
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(200).json(savedProduct);
+  } catch (error) {
+    res.status(402).json({ message: "something went wrong" });
+  }
+};
+
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await Products.find();
+    console.log(products);
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(402).json({ message: "Something went wrong" });
+  }
+};
