b4f1a1953532818cf9c63c690fe388da1931ae83
yetabalij <yetabalij@gmail.com>
2022-09-15 09:22:32 +0300

    product lists are featched.

--- src/Components/ProductLists.js
+++ src/Components/ProductLists.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Product from "./Product";
+import axios from "axios";
 
 const ProductLists = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/product").then((res) => {
+      setProducts(res.data);
+    });
+  }, []);
+
   return (
     <div>
-      <Product />
+      {products.map((product, key) => {
+        return (
+          <div key={key}>
+            <Product product={product} />
+          </div>
+        );
+      })}
     </div>
   );
 };
