aef6f1c768afcca02ec256953535704626a9bd70
yetabalij <yetabalij@gmail.com>
2022-09-15 09:21:54 +0300

    console removed.

--- server/controller/product.js
+++ server/controller/product.js
@@ -13,7 +13,6 @@ exports.createProduct = async (req, res) => {
 exports.getAllProducts = async (req, res) => {
   try {
     const products = await Products.find();
-    console.log(products);
     res.status(200).json(products);
   } catch (error) {
     res.status(402).json({ message: "Something went wrong" });
