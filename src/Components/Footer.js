d3285c895052e9cc44e719c7c1c18ad5ca222ef2
yetabalij <yetabalij@gmail.com>
2022-09-15 17:01:55 +0300

    footer started.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Logo from "./../assets/logo/yagermarket2.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="mt-10 bg-[#232f3e]">
+      <div className="w-[80%] mx-auto flex flex-row justify-between text-white py-12">
+        <div className="w-16 h-16">
+          <a href="/">
+            <img src={Logo} alt="Logo" />
+          </a>
+        </div>
+        <div>div 1</div>
+        <div>div 1</div>
+        <div>div 1</div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
