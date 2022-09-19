f2b8a15120c623d94d254f4261bc93591b786019
yetabalij <yetabalij@gmail.com>
2022-09-19 13:25:41 +0300

    width and marigin added. and no result found is implemented.

--- src/Components/ProductListContainer.js
+++ src/Components/ProductListContainer.js
@@ -4,16 +4,22 @@ import Product from "./Product";
 
 const ProductListContainer = ({ loading, searchData }) => {
   return (
-    <div className="flex flex-col items-center">
-      {loading && <Loading />}
-      <div className="grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
-        {searchData.map((product, key) => {
-          return (
-            <div key={key}>
-              <Product product={product} />
-            </div>
-          );
-        })}
+    <div className="w-[80%] mx-auto">
+      <div className="flex flex-col items-center">
+        {loading && <Loading />}
+        {searchData.length !== 0 ? (
+          <div className="grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+            {searchData.map((product, key) => {
+              return (
+                <div key={key}>
+                  <Product product={product} />
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <p>No result found.</p>
+        )}
       </div>
     </div>
   );
