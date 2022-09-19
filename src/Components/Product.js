20bec33113ce7d3e94c790741e6308de08e36443
yetabalij <yetabalij@gmail.com>
2022-09-19 11:10:58 +0300

    to check null value is implemented.

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -5,10 +5,10 @@ import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 const Product = ({ product }) => {
   return (
     <div className="bg-gray-50 shadow-md min-h-full">
-      <img src={product.image} alt={product.name} />
+      <img src={product?.image} alt={product?.name} />
       <div className="mx-2">
-        <h3 className="py-2 uppercase font-medium">{product.name}</h3>
-        <p>{`${product.description.substring(0, 150)} ...`}</p>
+        <h3 className="py-2 uppercase font-medium">{product?.name}</h3>
+        <p>{`${product?.description.substring(0, 150)} ...`}</p>
         <div className="my-2 text-orange-500">
           {
             <Rating
@@ -16,12 +16,12 @@ const Product = ({ product }) => {
               fullSymbol={<AiFillStar style={{ fontSize: "1.3rem" }} />}
               start={0}
               stop={5}
-              initialRating={product.ratting}
+              initialRating={product?.ratting}
               readonly
             />
           }
         </div>
-        <p className="mb-4 text-xl font-medium">{`$ ${product.price}`}</p>
+        <p className="mb-4 text-xl font-medium">{`$ ${product?.price}`}</p>
       </div>
     </div>
   );
