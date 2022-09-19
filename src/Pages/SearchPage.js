76d6afc71a6c200a0edad4c099bb6c66ba840b1c
yetabalij <yetabalij@gmail.com>
2022-09-19 11:11:54 +0300

    search by price is completed.

--- src/Pages/SearchPage.js
+++ src/Pages/SearchPage.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import NavBar from "../Components/NavBar";
+import ProductListContainer from "../Components/ProductListContainer";
 import { useLocation } from "react-router-dom";
 
 const SearchPage = () => {
   const location = useLocation();
+  const [searchData, setSearchData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const start = location.state.start;
+  const end = location.state.end;
+  const Value = {
+    start,
+    end,
+  };
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:5000/api/product/search", Value)
+      .then((res) => {
+        setSearchData(res.data.searchProduct);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
       <NavBar />
-      {location.state.start}
-      {location.state.end}
+      <ProductListContainer loading={loading} searchData={searchData} />
     </div>
   );
 };
