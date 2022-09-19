e68cacbc5ccfaff48ba7097e9668a51829e3c87d
yetabalij <yetabalij@gmail.com>
2022-09-19 14:39:28 +0300

    single product page added.

---
+++ src/Pages/SingleProduct.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../Components/NavBar";
+
+const SingleProduct = () => {
+  return (
+    <div>
+      <NavBar />
+      <p>Hello</p>
+    </div>
+  );
+};
+
+export default SingleProduct;
