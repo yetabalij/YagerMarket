1b69f71e60e121a0bbf4f061d85182a6b0d990fd
yetabalij <yetabalij@gmail.com>
2022-09-19 10:19:22 +0300

    route is added to search products with price.

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,11 +1,16 @@
 const express = require("express");
 
-const { getAllProducts, createProduct } = require("./../controller/product");
+const {
+  getAllProducts,
+  createProduct,
+  searchProducts,
+} = require("./../controller/product");
 
 const router = express.Router();
 
 //get all products
 router.get("/", getAllProducts);
 router.post("/createproduct", createProduct);
+router.post("/search", searchProducts);
 
 module.exports = router;
