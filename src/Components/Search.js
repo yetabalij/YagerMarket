87928af611a2fb7ce56901e162ef5ae38ee9e8ca
yetabalij <yetabalij@gmail.com>
2022-09-19 10:33:05 +0300

    filte with price is completed.

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Search = () => {
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(0);
   const navigate = useNavigate();
   return (
     <div className="flex items-center flex-col">
@@ -10,7 +12,7 @@ const Search = () => {
         <p
           className="hover:text-orange-400 cursor-pointer"
           onClick={() => {
-            navigate("/search", { state: { start: 25 } });
+            navigate("/search", { state: { start: 0, end: 25 } });
           }}
         >
           Under $25
@@ -23,13 +25,53 @@ const Search = () => {
         >
           $25 to $50
         </p>
-        <p className="hover:text-orange-400 cursor-pointer">$50 to $100</p>
-        <p className="hover:text-orange-400 cursor-pointer">$100 to $200</p>
-        <p className="hover:text-orange-400 cursor-pointer">$200 & above</p>
+        <p
+          className="hover:text-orange-400 cursor-pointer"
+          onClick={() => {
+            navigate("/search", { state: { start: 50, end: 100 } });
+          }}
+        >
+          $50 to $100
+        </p>
+        <p
+          className="hover:text-orange-400 cursor-pointer"
+          onClick={() => {
+            navigate("/search", { state: { start: 100, end: 200 } });
+          }}
+        >
+          $100 to $200
+        </p>
+        <p
+          className="hover:text-orange-400 cursor-pointer"
+          onClick={() => {
+            navigate("/search", { state: { start: 200, end: 5000 } });
+          }}
+        >
+          $200 & above
+        </p>
         <div className="flex gap-1">
-          <input className="border border-orange-500 border-solid w-[50px]" />
-          <input className="border border-orange-500 border-solid w-[50px]" />
-          <button className="bg-orange-500 text-white px-1">Go</button>
+          <input
+            className="border border-orange-500 border-solid w-[50px]"
+            placeholder="min"
+            onChange={(e) => {
+              setMin(e.target.value);
+            }}
+          />
+          <input
+            className="border border-orange-500 border-solid w-[50px]"
+            placeholder="max"
+            onChange={(e) => {
+              setMax(e.target.value);
+            }}
+          />
+          <button
+            className="bg-orange-500 text-white px-1"
+            onClick={() => {
+              navigate("/search", { state: { start: min, end: max } });
+            }}
+          >
+            Go
+          </button>
         </div>
       </div>
       <div className="w-full mt-5">
