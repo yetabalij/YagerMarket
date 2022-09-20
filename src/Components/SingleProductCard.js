3c4f527d0d72cc8ebb6b000b222192ec4f201d48
yetabalij <yetabalij@gmail.com>
2022-09-20 12:18:25 +0300

    single product card completed.

--- src/Components/SingleProductCard.js
+++ src/Components/SingleProductCard.js
@@ -1,7 +1,26 @@
 import React from "react";
 
-const SingleProductCard = () => {
-  return <div>SingleProductCard</div>;
+const SingleProductCard = ({ className }) => {
+  const product = JSON.parse(localStorage.getItem("singleProduct"));
+
+  return (
+    <div className={`p-4 shadow-xl ${className}`}>
+      <div className="">
+        <h3 className="text-lg uppercase font-medium">
+          {product.singleProduct.name}
+        </h3>
+        <span className="border-b-2 border-solid block border-gray-300 opacity-[0.6] w-[100%] my-2"></span>
+      </div>
+      <img
+        className="mb-4"
+        src={product.singleProduct.image}
+        alt={product.singleProduct.name}
+      />
+      <p className="justify-center content-center">
+        {product.singleProduct.description}
+      </p>
+    </div>
+  );
 };
 
 export default SingleProductCard;
