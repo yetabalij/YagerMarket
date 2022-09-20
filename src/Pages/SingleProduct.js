998f3a525665df142d9b2ba009879835e66ed602
yetabalij <yetabalij@gmail.com>
2022-09-20 09:38:45 +0300

    components are imported.

--- src/Pages/SingleProduct.js
+++ src/Pages/SingleProduct.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "../Components/NavBar";
+import SingleProductCard from "../Components/SingleProductCard";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import AddCartCard from "../Components/AddCartCard";
+import ReviewCard from "../Components/ReviewCard";
 
 const SingleProduct = () => {
+  const [loading, seatLoading] = useState(true);
+  const [singleProduct, setSingleProduct] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/product/singleproduct/${id}`)
+      .then((res) => {
+        setSingleProduct(res.data);
+        seatLoading(false);
+      });
+  }, []);
+
   return (
     <div>
       <NavBar />
-      <p>Hello</p>
+      <SingleProductCard />
+      <AddCartCard />
+      <ReviewCard />
     </div>
   );
 };
