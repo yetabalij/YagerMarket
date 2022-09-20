f541581498e890b75d2a01ce0eb555359cd79583
yetabalij <yetabalij@gmail.com>
2022-09-20 10:30:03 +0300

    cart icon is added and style modified.

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,20 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Logo from "./../assets/logo/yagermarket3.png";
+import { RiShoppingCartFill } from "react-icons/ri";
 
 const NavBar = () => {
   return (
     <div className="bg-gray-100 shadow-md mb-5">
       <div className="flex justify-between py-2 w-[94%] mx-auto items-center">
         <div className="w-16 h-16">
-          <a href="/">
+          <Link to="/">
             <img src={Logo} alt="Logo" />
-          </a>
+          </Link>
         </div>
-        <div className="flex gap-4">
-          <div>
-            <a href="/signin">SignIn</a>
+        <div className="flex gap-4 items-center">
+          <div className="text-xl font-medium text-orange-600">
+            <Link to="/signin">Sign In</Link>
           </div>
-          <div>cart</div>
+          <div className="text-orange-600">
+            <Link to="/cart" className="flex relative">
+              <RiShoppingCartFill className="text-3xl" />
+              <div className="absolute right-[-60%] top-[-20%] w-[18px] h-[18px]">
+                <p className="text-lg font-bold absolute top-[-15%]">1</p>
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
