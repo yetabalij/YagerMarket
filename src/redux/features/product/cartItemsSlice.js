ec47782d41d7c26246608aca51c6da594240788e
yetabalij <yetabalij@gmail.com>
2022-09-22 15:33:01 +0300

    add to cart action implemented.

--- src/redux/features/product/cartItemsSlice.js
+++ src/redux/features/product/cartItemsSlice.js
@@ -5,7 +5,24 @@ const cartItemsSlice = createSlice({
   initialState: {
     Items: [],
   },
-  reducers: {},
+  reducers: {
+    addItemToCart: (state, action) => {
+      const isExist = state.Items.find(
+        (item) => item._id === action.payload._id
+      );
+      if (isExist) {
+        return {
+          ...state,
+          Items: state.Items.map((item) =>
+            item._id === action.payload._id ? action.payload : item
+          ),
+        };
+      } else {
+        return { ...state, Items: [...state.Items, action.payload] };
+      }
+    },
+  },
 });
 
+export const { addItemToCart } = cartItemsSlice.actions;
 export default cartItemsSlice.reducer;
