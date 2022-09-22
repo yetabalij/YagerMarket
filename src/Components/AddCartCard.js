ee98f4295388aa7c56f066853738232492b56ce4
yetabalij <yetabalij@gmail.com>
2022-09-22 15:32:35 +0300

    action dispached.

--- src/Components/AddCartCard.js
+++ src/Components/AddCartCard.js
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addItemToCart } from "../redux/features/product/cartItemsSlice";
 
 const AddCartCard = () => {
+  const [amount, setAmount] = useState(1);
   const product = JSON.parse(localStorage.getItem("singleProduct"));
+  const dispach = useDispatch();
+
   return (
     <div className="sm:mt-4 shadow-sm p-4">
       <h3 className="text-lg font-medium">{`Price: $${product.singleProduct.price}`}</h3>
       <span className="border-b-2 border-solid block border-gray-300 opacity-[0.6] w-[100%] my-2"></span>
       <div className="mt-2">
         <h4 className="text-lg font-medium mb-1">Select Quantity</h4>
-        <select className="outline outline-offset-2 outline-1">
+        <select
+          className="outline outline-offset-2 outline-1"
+          onChange={(e) => {
+            setAmount(e.target.value);
+          }}
+        >
           {Array(product.singleProduct.quantity)
             .fill()
             .map((_, i) => {
-              return <option>{i + 1}</option>;
+              return <option value={i + 1}>{i + 1}</option>;
             })}
         </select>
       </div>
       <span className="border-b-2 border-solid block border-gray-300 opacity-[0.6] w-[100%] my-2"></span>
       <div className="py-4">
-        <button className="bg-orange-500 text-white px-12 py-2">
+        <button
+          className="bg-orange-500 text-white px-12 py-2"
+          onClick={() => {
+            dispach(addItemToCart({ ...product.singleProduct, amount }));
+          }}
+        >
           Add to Cart
         </button>
       </div>
