deb22bb9f5d026596560d7112f4aea2cadb99c92
yetabalij <yetabalij@gmail.com>
2022-09-22 10:12:24 +0300

    slice added.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./features/product/productSlice";
+import cartItemsSlice from "./features/product/cartItemsSlice";
 
 export const store = configureStore({
   reducer: {
     Products: productReducer,
+    CartItems: cartItemsSlice,
   },
 });
