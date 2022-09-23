94c6a5b340db0cd155c282eb7608a022f0bebd90
yetabalij <yetabalij@gmail.com>
2022-09-23 14:28:35 +0300

    new route added and code snippet commented.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+// import { useEffect } from "react";
+import { itemToLocalStorate } from "./redux/features/product/cartItemsSlice";
 import Home from "./Pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//Components to route
 import Footer from "./Components/Footer";
 import SearchPage from "./Pages/SearchPage";
 import SingleProduct from "./Pages/SingleProduct";
+import Cart from "./Pages/Cart";
 
 function App() {
+  // useEffect(() => {
+  //   itemToLocalStorate();
+  // }, []);
   return (
     <div>
       <BrowserRouter>
@@ -12,6 +20,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<SearchPage />} />
           <Route path="/singleproduct/:id" element={<SingleProduct />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </BrowserRouter>
       <Footer />
