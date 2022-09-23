964061747d52efe695d557a3e5f16569418ef672
yetabalij <yetabalij@gmail.com>
2022-09-23 15:09:14 +0300

    data fetched from localstorage.

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,11 +1,15 @@
-import React from "react";
 import NavBar from "../Components/NavBar";
 
 const Cart = () => {
+  const cartItems = JSON.parse(localStorage.getItem("cartItems"));
+
   return (
     <div>
       <NavBar />
       Cart
+      {cartItems?.map((item) => {
+        return <h3>{item.name}</h3>;
+      })}
     </div>
   );
 };
