70aa5d1e5d31c6f9dc474dbd911eb27da649c062
yetabalij <yetabalij@gmail.com>
2022-10-10 10:46:48 +0300

    two way data binding.

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,49 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Footer from "../Components/Footer";
 import NavBar from "./../Components/NavBar";
 
 const Signup = () => {
   const navigate = useNavigate();
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const formValue = {
+    fname,
+    lname,
+    email,
+    password,
+    confirmPassword,
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    alert(JSON.stringify(formValue));
+  };
   return (
     <div>
       <NavBar />
       <div className="flex flex-col">
         <div className="border border-solid border-orange-300 drop-shadow-sm w-[90%] md:w-[45%] lg:w-[40%] xl:w-[30%] 2xl:w-[25%] p-5 mx-auto">
           <p className="text-2xl font-medium mb-3">Sign up</p>
-          <form className="flex flex-col">
+          <form className="flex flex-col" onSubmit={onSubmitHandler}>
             <label className="font-bold">First Name</label>
             <input
               className="border border-gray-400 rounded focus:outline-none focus:border-orange-400 p-2 mb-1 text-gray-600"
               type="text"
               placeholder="First Name"
+              value={fname}
+              onChange={(e) => {
+                setFname(e.target.value);
+              }}
             />
             <label className="font-bold">Last Name</label>
             <input
               className="border border-gray-400 rounded active:border-orange-400 focus:outline-none focus:border-orange-400 p-2 mb-1 text-gray-600"
               type="text"
               placeholder="Last Name"
+              value={lname}
+              onChange={(e) => {
+                setLname(e.target.value);
+              }}
             />
             <label className="font-bold">Email</label>
             <input
               className="border border-gray-400 rounded focus:outline-none focus:border-orange-400 p-2 mb-1 text-gray-600"
               type="text"
               placeholder="Email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
             />
             <label className="font-bold">Password</label>
             <input
               className="border border-gray-400 rounded focus:outline-none focus:border-orange-400 p-2 mb-1 text-gray-600"
               type="password"
               placeholder="Password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
             />
             <label className="font-bold">Confirm Password</label>
             <input
               className="border border-gray-400 rounded focus:outline-none focus:border-orange-400 p-2 mb-1 text-gray-600"
               type="password"
               placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => {
+                setConfirmPassword(e.target.value);
+              }}
             />
             <div>
-              <button className="mt-2 w-full bg-orange-500 text-white py-2 rounded-sm">
+              <button
+                className="mt-2 w-full bg-orange-500 text-white py-2 rounded-sm"
+                type="submit"
+              >
                 Create Account
               </button>
             </div>
