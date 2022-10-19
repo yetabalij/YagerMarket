b1e469ad031d0bf649ef73631a92181262ce0df2
yetabalij <yetabalij@gmail.com>
2022-10-19 09:37:44 +0300

    user model is created.

---
+++ server/model/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    require: true,
+  },
+  lastName: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+});
+
+module.exports = mongoose.Model("User", userSchema);
